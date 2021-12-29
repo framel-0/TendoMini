b1af65bc2d58b7b88f95653e105860910a4a56c7
Frank Melomey <melomeyfrank@gmail.com>
2021-12-29 21:39:04 +0000

    splash screen update

--- tendo/app/src/main/java/com/example/tendomini/ui/SplashActivity.kt
+++ tendo/app/src/main/java/com/example/tendomini/ui/SplashActivity.kt
@@ -1,12 +1,44 @@
 package com.example.tendomini.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tendomini.R
+import com.example.tendomini.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val anim = AnimationUtils.loadAnimation(
+            applicationContext,
+            R.anim.fade_in
+        ) // Create the animation.
+
+        anim!!.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+            override fun onAnimationEnd(animation: Animation) {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+        })
+
+        binding.imageSplash.startAnimation(anim)
+
     }
 }
